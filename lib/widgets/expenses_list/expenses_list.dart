1e6df3dea20571a2d52c36d118be0e64e8b48f06
Artur <artur@Arturs-MacBook-Pro.local>
2024-07-17 10:24:51 +0200

    Add custom list item with card and spacer widgets

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
@@ -15,7 +16,9 @@ class ExpensesList extends StatelessWidget {
     // of performance and visability. ListView creates scrolable list.
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (context, index) => Text(expenses[index].title),
+      itemBuilder: (context, index) => ExpenseItem(
+        expenses[index],
+      ),
     );
   }
 }
