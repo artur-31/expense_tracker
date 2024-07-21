c1be91fd4b76d627c8a0bbd82ac38107b33c9f82
martirosianartur <martirosianartur4@gmail.com>
2024-07-21 12:41:21 +0200

    Add modal for expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -19,11 +20,19 @@ class _ExpensesState extends State<Expenses> {
         date: DateTime.now(),
         category: Category.work),
     Expense(
-        title: 'Cinema',
-        amount: 12.99,
-        date: DateTime.now(),
-        category: Category.leisure)
+      title: 'Cinema',
+      amount: 12.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    )
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +41,7 @@ class _ExpensesState extends State<Expenses> {
         title: const Text('Flutter Expense Tracker'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _openAddExpenseOverlay,
             icon: const Icon(Icons.add),
           ),
         ],
